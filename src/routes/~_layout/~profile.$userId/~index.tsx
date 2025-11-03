4d7f0b1dbb8278a63befedad4b4880e0930a73af
TheBluemist1404 <hoang.ct1lhp@gmail.com>
2025-11-03 18:42:42 +0700

    feat: random commit

--- src/routes/~_layout/~profile.$userId/~index.tsx
+++ src/routes/~_layout/~profile.$userId/~index.tsx
@@ -6,5 +6,14 @@ export const Route = createFileRoute('/_layout/profile/$userId/')({
 
 function RouteComponent() {
   const { userId } = Route.useParams()
-  return <div>Hello user with id {userId}</div>
+  return (
+    <div>
+      <p>Hello user with id {userId}</p>
+      <select name="" id="" className="bg-white p-2 ">
+        <option value="">1</option>
+        <option value="">2</option>
+        <option value="">3</option>
+      </select>
+    </div>
+  )
 }
