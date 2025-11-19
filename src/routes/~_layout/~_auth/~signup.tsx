3de1c7a8880541ec85368ac769d3528759411001
Toan Phan <toanfphk@gmail.com>
2025-11-19 20:50:47 +0700

    fix: make the signup page more responsive and flexible

--- src/routes/~_layout/~_auth/~signup.tsx
+++ src/routes/~_layout/~_auth/~signup.tsx
@@ -13,11 +13,11 @@ export const Route = createFileRoute('/_layout/_auth/signup')({
 
 function RouteComponent() {
   return (
-    <div className="flex min-h-screen bg-cloudwhite">
+    <div className="flex h-full bg-cloudwhite">
       {/* LEFT */}
       <Link
         to="/"
-        className="absolute top-25 left-10 mb-10 inline-flex items-center gap-2 text-sm text-gray-500 hover:text-gray-700"
+        className="absolute top-6 sm:top-8 md:top-10 left-6 sm:left-8 md:left-10 z-10 inline-flex items-center gap-2 text-sm text-gray-500 hover:text-gray-700"
       >
         <img src={arrowIcon} alt="Back" className="h-4 w-4 md:h-5 md:w-5" />
         <span className="underline-offset-2 hover:underline">
@@ -25,54 +25,54 @@ function RouteComponent() {
         </span>
       </Link>
 
-      <div className="flex w-full flex-col bg-cloudwhite px-8 py-8 md:w-1/2 lg:px-20 lg:py-14 justify-center items-center">
+      <div className="flex flex-1 flex-col bg-cloudwhite px-8 py-8 lg:px-20 lg:py-14 justify-center items-center">
         <div className="max-w-md">
-          <h1 className="mb-10 text-5xl font-semibold leading-tight text-[#3b2f09]">
+          <h1 className="mb-8 sm:mb-10 text-3xl sm:text-4xl lg:text-5xl font-semibold leading-tight text-[#3b2f09]">
             Create an account
           </h1>
 
-          <form className="space-y-8">
+          <form className="space-y-6 sm:space-y-8">
             {/* Email */}
-            <div className="relative">
+            <div className="flex items-center">
               <img
                 src={mailIcon}
                 alt="Email"
-                className="pointer-events-none absolute left-0 top-1/2 h-5 w-5 -translate-y-2 translate-x-2 text-gray"
+                className="h-5 w-5 text-gray mr-2"
               />
               <input
                 id="email"
                 type="email"
-                className="w-full h-10 border-b border-gray-300 bg-transparent py-2 pl-10 text-base outline-none focus:border-gray focus:text-gray-900 focus:shadow-sm"
+                className="flex-1 border-b border-gray-300 bg-transparent p-2 text-base outline-none focus:border-gray focus:text-gray-900 focus:shadow-sm"
                 placeholder="Email"
               />
             </div>
 
             {/* Username */}
-            <div className="relative">
+            <div className="flex items-center">
               <img
                 src={userIcon}
                 alt="Username"
-                className="pointer-events-none absolute left-0 top-1/2 h-5 w-5 -translate-y-2 translate-x-2 text-gray"
+                className="h-5 w-5 text-gray mr-2"
               />
               <input
                 id="username"
                 type="text"
-                className="mt-2 w-full border-b border-gray-300 bg-transparent py-2 pl-10 text-base outline-none focus:border-gray focus:text-gray-900 focus:shadow-sm"
+                className="flex-1 border-b border-gray-300 bg-transparent p-2 text-base outline-none focus:border-gray focus:text-gray-900 focus:shadow-sm"
                 placeholder="Username"
               />
             </div>
 
             {/* Password */}
-            <div className="relative">
+            <div className="flex items-center">
               <img
                 src={lockIcon}
                 alt="Password"
-                className="pointer-events-none absolute left-0 top-1/2 h-5 w-5 -translate-y-2 translate-x-2 text-gray"
+                className="h-5 w-5 text-gray mr-2"
               />
               <input
                 id="password"
                 type="password"
-                className="mt-2 w-full border-b border-gray-300 bg-transparent py-2 pl-10 text-base outline-none focus:border-gray focus:text-gray-900 focus:shadow-sm"
+                className="flex-1 border-b border-gray-300 bg-transparent p-2 text-base outline-none focus:border-gray focus:text-gray-900 focus:shadow-sm"
                 placeholder="Password"
               />
             </div>
@@ -80,7 +80,7 @@ function RouteComponent() {
             <div className="mt-6 flex flex-wrap items-center gap-4">
               <button
                 type="submit"
-                className="rounded-full w-75 bg-yellow px-10 py-3 text-sm font-semibold text-black shadow-md transition hover:brightness-110 cursor-pointer"
+                className="rounded-full bg-yellow flex-grow px-12 py-3 text-sm font-semibold text-black shadow-md transition hover:brightness-110 cursor-pointer sm:flex-grow-0 sm:px-16 sm:min-w-[300px] md:px-20"
               >
                 Sign up
               </button>
@@ -97,7 +97,7 @@ function RouteComponent() {
             </div>
           </form>
 
-          <p className="mt-10 text-sm text-gray-600 justify-center flex">
+          <p className="mt-8 sm:mt-10 text-sm text-gray-600 justify-center flex">
             Already have an account?
             <Link
               to="/login"
@@ -111,17 +111,17 @@ function RouteComponent() {
 
       {/* RIGHT */}
       <div
-        className="relative hidden w-1/2 bg-cover bg-center md:block"
+        className="relative hidden bg-cover bg-center md:block md:w-1/2"
         style={{ backgroundImage: `url(${bkuImg})` }}
       >
         <div className="absolute inset-0 bg-wood/80" />
 
-        <div className="relative z-10 flex h-full items-center justify-center">
-          <div className="flex items-center justify-center rounded-full w-200">
+        <div className="relative z-10 flex h-full items-center justify-center p-10">
+          <div className="flex items-center justify-center rounded-full w-full lg:max-w-[900px]">
             <img
               src={homeImg}
               alt="Lost and found illustration"
-              className="h-[100%] w-[100%] object-contain"
+              className="h-full w-full object-contain"
             />
           </div>
         </div>
